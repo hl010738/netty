facd50ed85074f78db9c09e3f46f433873d74804
talent.he <hl010738@126.com>
2018-03-01 19:11:08 +0800

    NIO

---
+++ src/main/java/com/netty/nio/NioServer.java
@@ -0,0 +1,68 @@
+package com.netty.nio;
+
+
+import com.google.common.collect.Maps;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * NIO 网络编程
+ * 多个客户端同时绑定到一个端口
+ */
+public class NioServer {
+
+    private static Map<String, SocketChannel> clientMap = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(Boolean.FALSE);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(8899);
+        serverSocketChannel.bind(inetSocketAddress);
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (Boolean.TRUE){
+            selector.select();
+            Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+            selectionKeySet.forEach(x -> {
+                try {
+                    if(x.isAcceptable()){
+                        ServerSocketChannel server = (ServerSocketChannel) x.channel();
+                        SocketChannel client = server.accept();
+                        client.configureBlocking(Boolean.FALSE);
+                        client.register(selector, SelectionKey.OP_READ);
+
+                        String key = UUID.randomUUID().toString();
+                        clientMap.put(key, client);
+                    } else if (x.isReadable()){
+                        SocketChannel client = (SocketChannel) x.channel();
+                        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                        int count = client.read(readBuffer);
+
+                        if (count > 0){
+                            readBuffer.flip();
+                            Charset charset = Charset.forName("utf-8");
+                            String receiveMessage = String.valueOf(charset.decode(readBuffer).array());
+
+                            System.out.println(client + ": " + receiveMessage);
+                        }
+
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
