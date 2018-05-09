b1317b07daf2ed05f8acffd968d14a31e034913f
talent.he <hl010738@126.com>
2018-05-09 16:47:46 +0800

    如果此服务端接收到客户端发送过来的消息，然后再转发到其他的服务器时 不应new一个新的eventLoop，应该使用当前的eventLoop.

--- src/main/java/com/netty/chat/example/ServerHandler.java
+++ src/main/java/com/netty/chat/example/ServerHandler.java
@@ -1,5 +1,6 @@
 package com.netty.chat.example;
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -28,6 +29,15 @@ public class ServerHandler extends SimpleChannelInboundHandler<String> {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         Channel channel = ctx.channel();
+        /**
+         *
+         这里的代码是：如果此服务端接收到客户端发送过来的消息，然后再转发到其他的服务器时
+         不应new一个新的eventLoop，应该使用当前的eventLoop.
+
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(ctx.channel().eventLoop());
+         */
         System.out.println(channel.remoteAddress() + " 上线");
     }
 
