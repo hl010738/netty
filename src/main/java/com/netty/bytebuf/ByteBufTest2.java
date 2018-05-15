639499921cea88744f8df96c1f26cbb6894b9761
talent.he <hl010738@126.com>
2018-05-15 14:39:06 +0800

    Netty ByteBuf

---
+++ src/main/java/com/netty/bytebuf/ByteBufTest2.java
@@ -0,0 +1,18 @@
+package com.netty.bytebuf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+
+public class ByteBufTest2 {
+    public static void main(String[] args) {
+        CompositeByteBuf compositeByteBuf = Unpooled.compositeBuffer();
+
+        ByteBuf heapBuf = Unpooled.buffer(10);
+        ByteBuf directBuf = Unpooled.directBuffer(10);
+
+        compositeByteBuf.addComponents(heapBuf, directBuf);
+
+        compositeByteBuf.forEach(System.out::println);
+    }
+}
